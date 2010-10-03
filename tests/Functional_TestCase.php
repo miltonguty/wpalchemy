25bae23bf01905a2ea2f2fdf2848c80b149cef43
Dimas Begunoff <drb@farinspace.com>
2010-10-02 23:39:28 -0700

    added additional tests to functional testcase

--- tests/Functional_TestCase.php
+++ tests/Functional_TestCase.php
@@ -22,12 +22,18 @@ class WPAlchemy_Functional_TestCase extends WPAlchemy_Base_TestCase
 		$this->_use_config($this->_config);
 	}
 
-	function _do_test()
+	function _mb_is_present()
 	{
-		$this->assertEquals('1', $this->getEval("this.browserbot.getUserWindow().jQuery('#" . $this->_id . "_metabox').length;"));
+		return (int)$this->getEval("this.browserbot.getUserWindow().jQuery('#" . $this->_id . "_metabox').length;") ? TRUE : FALSE ;
+	}
 
-		// simple contents text
-		$this->assertEquals('Name', $this->getEval("this.browserbot.getUserWindow().jQuery('#" . $this->_id . "_metabox .inside label:eq(0)').text();"));
+	function _mb_has_content()
+	{
+		$text = $this->getEval("this.browserbot.getUserWindow().jQuery('#" . $this->_id . "_metabox .inside label:eq(0)').text();");
+
+		if ('Name' === $text) return TRUE;
+
+		return FALSE;
 	}
 
 	### is meta box displayed (existing post/page)
@@ -38,7 +44,9 @@ class WPAlchemy_Functional_TestCase extends WPAlchemy_Base_TestCase
 
 		$this->_use_post_type();
 
-		$this->_do_test();
+		$this->assertTrue($this->_mb_is_present());
+
+		$this->assertTrue($this->_mb_has_content());
 	}
 
 	function test_regular_login_and_page_type()
@@ -47,7 +55,9 @@ class WPAlchemy_Functional_TestCase extends WPAlchemy_Base_TestCase
 
 		$this->_use_page_type();
 
-		$this->_do_test();
+		$this->assertTrue($this->_mb_is_present());
+
+		$this->assertTrue($this->_mb_has_content());
 	}
 
 	### is meta box displayed (new post/page)
@@ -58,7 +68,9 @@ class WPAlchemy_Functional_TestCase extends WPAlchemy_Base_TestCase
 
 		$this->_use_post_type_new();
 
-		$this->_do_test();
+		$this->assertTrue($this->_mb_is_present());
+
+		$this->assertTrue($this->_mb_has_content());
 	}
 
 	function test_regular_login_and_page_type_new()
@@ -67,7 +79,99 @@ class WPAlchemy_Functional_TestCase extends WPAlchemy_Base_TestCase
 
 		$this->_use_page_type_new();
 
-		$this->_do_test();
+		$this->assertTrue($this->_mb_is_present());
+
+		$this->assertTrue($this->_mb_has_content());
+	}
+
+	function test_regular_login_and_post_type_and_publish()
+	{
+		$this->_use_regular_login();
+
+		$this->_use_post_type();
+
+		$this->assertTrue($this->_mb_is_present());
+
+		// save data
+
+		$this->type($this->_id . '[name]', 'test title');
+
+		$this->type($this->_id . '[description]', 'test desc');
+
+		$this->click("publish");
+
+		$this->waitForPageToLoad('30000');
+
+		// check
+
+		$this->_use_post_type();
+
+		$this->assertEquals("test title", $this->getValue($this->_id . '[name]'));
+
+		$this->assertEquals("test desc", $this->getValue($this->_id . '[description]'));
+
+		// cleanup data
+
+		$this->type($this->_id . '[name]', '');
+
+		$this->type($this->_id . '[description]', '');
+
+		$this->click("publish");
+
+		$this->waitForPageToLoad('30000');
+
+		// check
+
+		$this->_use_post_type();
+
+		$this->assertEquals('', $this->getValue($this->_id . '[name]'));
+
+		$this->assertEquals('', $this->getValue($this->_id . '[description]'));
+	}
+
+	function test_regular_login_and_page_type_and_publish()
+	{
+		$this->_use_regular_login();
+
+		$this->_use_page_type();
+
+		$this->assertTrue($this->_mb_is_present());
+
+		// save data
+
+		$this->type($this->_id . '[name]', 'test title');
+
+		$this->type($this->_id . '[description]', 'test desc');
+
+		$this->click("publish");
+
+		$this->waitForPageToLoad('30000');
+
+		// check
+
+		$this->_use_page_type();
+
+		$this->assertEquals("test title", $this->getValue($this->_id . '[name]'));
+
+		$this->assertEquals("test desc", $this->getValue($this->_id . '[description]'));
+
+		// cleanup data
+
+		$this->type($this->_id . '[name]', '');
+
+		$this->type($this->_id . '[description]', '');
+
+		$this->click("publish");
+
+		$this->waitForPageToLoad('30000');
+
+		// check
+
+		$this->_use_page_type();
+
+		$this->assertEquals('', $this->getValue($this->_id . '[name]'));
+
+		$this->assertEquals('', $this->getValue($this->_id . '[description]'));
 	}
 }
 
